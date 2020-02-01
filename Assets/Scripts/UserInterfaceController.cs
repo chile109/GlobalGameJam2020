051d0514c336fd77aad5af35bdb2c13a3612c812
KevinChang <kevin.chang@aktsk.com>
2020-02-01 20:21:14 +0800

    Add Fade In/Out Effect

---
+++ Assets/Scripts/UserInterfaceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterfaceController : MonoBehaviour
+{
+    public Image CrossEfect;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CrossScene();
+        }
+    }
+
+    public void CrossScene()
+    {
+        var fadeOut = CrossEfect.DOFade(1, 1f);
+        var fadeIn = CrossEfect.DOFade(0, 1f);
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(fadeOut);
+        mySequence.Append(fadeIn);
+        
+    }
+}
