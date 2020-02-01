32cef7383f32a29ffd6d1abdd4bd04e259992ba0
Wallace.Hsieh <wallace.dev.des@gmail.com>
2020-02-01 21:17:15 +0800

    Add wire manipulation

---
+++ Assets/Scripts/Wire/DragPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Joint))]
+public class DragPoint : MonoBehaviour
+{
+    private Vector3 m_screenPoint, m_offset;
+    public Rigidbody Body
+    {
+        get; private set;
+    }
+    private Joint m_joint;
+
+    private void Awake()
+    {
+        Body = GetComponent<Rigidbody>();
+        m_joint = GetComponent<Joint>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        m_offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, m_screenPoint.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, m_screenPoint.z);
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        Body.MovePosition(mouseWorldPos + m_offset);
+    }
+}
---
+++ Assets/Scripts/Wire/Wire.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider), typeof(Rigidbody))]
+public class Wire : MonoBehaviour
+{
+    private Vector3 m_screenPoint, m_offset;
+    private Collider m_collider;
+    public Rigidbody WireRigidbody
+    {
+        get; private set;
+    }
+    private ConfigurableJoint m_joint;
+    [SerializeField]
+    private Wire m_parentWire;
+    private Rigidbody m_jointConnected;
+    [SerializeField]
+    private DragPoint m_pullerPrefab;
+    private DragPoint m_puller;
+    [SerializeField]
+    private Vector3 m_anchorValue;
+
+    private void Awake()
+    {
+        m_collider = GetComponent<Collider>();
+        WireRigidbody = GetComponent<Rigidbody>();
+        m_joint = GetComponent<ConfigurableJoint>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_joint.connectedBody = m_parentWire?.WireRigidbody;
+        m_screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        m_puller = Instantiate(m_pullerPrefab, transform.position, Quaternion.identity);
+        StartRecursiveInverseConnect(m_puller.Body);
+    }
+
+    public void OnMouseUp()
+    {
+        Destroy(m_puller.gameObject);
+        EndRecursiveInverseConnect();
+    }
+
+    public void OnMouseDrag()
+    {
+        //m_joint.targetPosition = m_puller.transform.position;
+    }
+
+    public void StartRecursiveInverseConnect(Rigidbody rb)
+    {
+        if (m_joint.connectedBody != null)
+        {
+            m_parentWire.RecursiveInverseConnect(this.WireRigidbody);
+        }
+        m_joint.anchor = Vector3.zero;
+        m_joint.connectedBody = rb;
+    }
+
+    public void EndRecursiveInverseConnect()
+    {
+        m_joint.anchor = m_anchorValue;
+        m_joint.connectedBody = m_parentWire?.WireRigidbody;
+        m_parentWire?.EndRecursiveInverseConnect();
+    }
+
+    public void RecursiveInverseConnect(Rigidbody rb)
+    {
+        if (m_joint.connectedBody != null)
+        {
+            m_parentWire.RecursiveInverseConnect(this.WireRigidbody);
+        }
+        m_joint.anchor = -m_anchorValue;
+        m_joint.connectedBody = rb;
+    }
+}
