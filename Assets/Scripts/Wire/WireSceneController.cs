3e3706d091c123d3a76abdb4c37fb9099cc61358
Wallace.Hsieh <wallace.dev.des@gmail.com>
2020-02-02 14:45:22 +0800

    Fix duplicate camera

--- Assets/Scripts/Wire/WireSceneController.cs
+++ Assets/Scripts/Wire/WireSceneController.cs
@@ -5,10 +5,21 @@ public class WireSceneController : MonoBehaviour
 {
     public static WireSceneController I;
     public bool HasWon = false;
+    public Transform CameraPos;
+
+    private void Awake()
+    {
+        if (!Camera.main.enabled)
+            Camera.main.enabled = true;
+        Camera.main.transform.SetPositionAndRotation(CameraPos.position, CameraPos.rotation);
+    }
 
     private void Start()
     {
         I = this;
+        if (!Camera.main.enabled)
+            Camera.main.enabled = true;
+        Camera.main.transform.SetPositionAndRotation(CameraPos.position, CameraPos.rotation);
     }
 
     public void WinStage()
